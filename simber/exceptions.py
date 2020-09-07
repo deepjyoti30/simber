5b87ea9c50816938489a3cc726548187ca318160
Deepjyoti Barman <deep.barman30@gmail.com>
2020-09-07 18:21:27 +0530

    Make some change to the InvalidLevel exception
    The InvalidLevel exception class will just take the level that the user
    passed which is invalid. It will then use that message to build a valid
    message.

--- simber/exceptions.py
+++ simber/exceptions.py
@@ -9,7 +9,7 @@ class InvalidLevel(Exception):
     This will be raise if the level the user
     passed is not valid.
     """
-    def __init__(self, message, level):
+    def __init__(self, level):
         super().__init__()
 
         self.message = self._build_message(level)
@@ -17,10 +17,10 @@ class InvalidLevel(Exception):
     def _build_message(self, level):
         """Build a message to show the user"""
         message = "{level}: is an invalid level."\
-                  "Try one of these: {all_levels}"
+                  " Try one of these: {all_levels}"
         return message.format(
             level=level,
-            all_levels=dict(Default().level_number.keys())
+            all_levels=list(Default().level_number.keys())
         )
 
     def __str__(self):
