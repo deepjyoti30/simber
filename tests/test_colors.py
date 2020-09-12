0681ee41bb7f7449f97037c85930ea8d31d625aa
Deepjyoti Barman <deep.barman30@gmail.com>
2020-09-12 11:20:06 +0530

    Add test for the colors module
    Add tests for the colors module. The colors module is really important
    so it is important to make sure everything is working all right without
    any issues.

---
+++ tests/test_colors.py
@@ -0,0 +1,33 @@
+"""Test the colors module of the logger."""
+
+from simber.colors import ColorFormatter
+from colorama import Fore
+
+
+def test_color_formatter():
+    """Test the color_formatter method of the class"""
+    s = "%gnana%"
+    output = Fore.GREEN + "nana" + Fore.RESET
+
+    assert ColorFormatter.format_colors(s) == output, "Should be `Green`"
+
+
+def test__replace_with_colors():
+    """Test the _replace_with_colors method"""
+    s = "%gnana%"
+    output = Fore.GREEN + "nana" + Fore.RESET
+
+    color_formatter = ColorFormatter()
+
+    assert color_formatter._replace_with_colors(s) == output, "Should be `Green`"
+
+
+def test__get_color_replacement():
+    """Test the _get_color_replacement method"""
+    s = "%rnana%"
+    output = Fore.RED + "nana" + Fore.RESET
+
+    color_formatter = ColorFormatter()
+    output_returned = color_formatter._get_color_replacement(s)
+
+    assert output_returned == output, "Should be {}, got {}".format(output, output_returned)
