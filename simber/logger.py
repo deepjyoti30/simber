6c777de27aabee479f83834640191f3ffce7bd6c
Deepjyoti Barman <deep.barman30@gmail.com>
2020-12-03 20:29:16 +0530

    Add method to update the log level of files

--- simber/logger.py
+++ simber/logger.py
@@ -218,6 +218,7 @@ class Logger(object):
         Update all the streams with the passed level.
 
         This will not update the level of file outputs.
+        Use `update_file_level` for updating files
         """
         # First check if the passed level is present in the supported ones
         if level not in self._level_number:
@@ -228,6 +229,23 @@ class Logger(object):
         # Update the level for only stdout outputs
         for stream in self._streams:
             if stream.stream_name in valid_names:
+                stream.level = self._level_number[level]
+
+    def update_file_level(self, level):
+        """
+        Update the level of all the file streams.
+
+        This will update the log level of all the file streams.
+        """
+        # First check if the passed level is present in the supported ones
+        if level not in self._level_number:
+            raise InvalidLevel(level)
+
+        valid_names = Default().valid_stdout_names
+
+        # Update the level for only stdout outputs
+        for stream in self._streams:
+            if stream.stream_name not in valid_names:
                 stream.level = self._level_number[level]
 
     def update_disable_file(self, disable_file):
